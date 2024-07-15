be82afcc2fe116c01bf42d9adb277294413c5e81
mdsiaofficial <mdsiaofficial@gmail.com>
2024-07-15 15:44:54 +0600

    "Added Socket.IO library and implemented WebSocket connection in chat app"

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const { Server } = require('socket.io')
+
+
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 
-app.use(express.static('./public'))
+
+
+// socket io
+io.on('connection', (socket) => {
+  console.log("Connection detected");
+  console.log(`Socket ID: ${socket.id}`);
+});
+
+app.use(express.static(path.resolve('./public')))
 
 app.get('/', (req, res) => {
-  return res.sendFile("./public/index.html")
+  return res.sendFile("/public/index.html")
 })
 server.listen(9000, () => console.log(`Server Started at PORT: 9000 http://localhost:9000`));
 
